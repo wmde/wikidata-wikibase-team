6832a88d2ce511e4c0275631421caa5ae20f9bde
addshore <addshorewiki@gmail.com>
2021-04-21 09:36:18 +0100

    Use a "fancy" table for our repositories

--- .vuepress/components/Repositories.vue
+++ .vuepress/components/Repositories.vue
@@ -1,13 +1,30 @@
 <template>
   <div>
+
     <h2>Ours</h2>
-    <ul>
-      <li v-for="data in repositories['main']"><a v-bind:href="data">{{data}}</a></li>
-    </ul>
+    <table>
+      <tr>
+        <th>Location</th>
+        <th>Repository</th>
+      </tr>
+    <tr v-for="data in repositories['main']">
+      <td>
+        <a v-if="data.includes('gerrit.wikimedia.org')" v-bind:href="data">
+          <img width="20" height="20" src="https://gerrit.wikimedia.org/favicon.ico"></img> Gerrit
+          </a>
+        <a v-if="data.includes('github.com')" v-bind:href="data">
+          <img width="20" height="20" src="https://github.com/favicon.ico"></img> Github
+          </a>
+      </td>
+      <td><a v-bind:href="data">{{data.replace('https://gerrit.wikimedia.org/r/admin/repos/','').replace('https://github.com/','')}}</a></td>
+    </tr>
+    </table>
+
     <h2>Others</h2>
     <ul>
       <li v-for="data in repositories['related']"><a v-bind:href="data">{{data}}</a></li>
     </ul>
+
   </div>
 </template>
 
