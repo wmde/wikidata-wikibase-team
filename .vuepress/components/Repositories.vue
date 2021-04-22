12678bed9e721b0afee845d84c0c74a7ba0e73c2
addshore <addshorewiki@gmail.com>
2021-04-22 10:16:28 +0100

    Use vuetify data table...

--- .vuepress/components/Repositories.vue
+++ .vuepress/components/Repositories.vue
@@ -1,8 +1,28 @@
 <template>
   <div>
 
-    <h2>Ours</h2>
-    <table>
+    <v-data-table
+      :headers="headers"
+      :items="mainRepositories"
+      :items-per-page="100"
+      class="elevation-1"
+    >
+      <template v-slot:item.location="{ item }">
+        <a v-if="item.location === 'gerrit'" v-bind:href="item.url">
+          <img width="20" height="20" src="https://gerrit.wikimedia.org/favicon.ico"></img> Gerrit
+          </a>
+        <a v-if="item.location === 'github'" v-bind:href="item.url">
+          <img width="20" height="20" src="https://github.com/favicon.ico"></img> Github
+        </a>
+      </template>
+      <template v-slot:item.name="{ item }">
+        <a v-bind:href="item.url">
+          {{item.name}}
+        </a>
+      </template>
+
+    </v-data-table>
+<!--     
       <tr>
         <th>Location</th>
         <th>Repository</th>
@@ -18,17 +38,6 @@
       </tr>
     <tr v-for="repo in repositories['main']" :key="repo.url">
       <td>
-        <a v-if="repo.url.includes('gerrit.wikimedia.org')" v-bind:href="repo.url">
-          <img width="20" height="20" src="https://gerrit.wikimedia.org/favicon.ico"></img> Gerrit
-          </a>
-        <a v-if="repo.url.includes('github.com')" v-bind:href="repo.url">
-          <img width="20" height="20" src="https://github.com/favicon.ico"></img> Github
-          </a>
-      </td>
-      <td>
-        <a v-bind:href="repo.url">{{repo.url.replace('https://gerrit.wikimedia.org/r/admin/repos/','').replace('https://github.com/','')}}</a>
-      </td>
-      <td>
         tba...
         <img v-if="repo.languages && repo.languages.indexOf('php') > -1" width="20" height="20" src="https://php.net/favicon.ico">
         <img v-if="repo.languages && repo.languages.indexOf('js') > -1" width="20" height="20" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Javascript-736400_960_720.png/20px-Javascript-736400_960_720.png">
@@ -40,8 +49,7 @@
       <td>
         <img v-if="repo.tools && repo.tools.indexOf('composer') > -1" width="20" height="20" src="https://getcomposer.org/favicon.ico">
       </td>
-    </tr>
-    </table>
+    </tr> -->
 
     <h2>Others</h2>
     <ul>
@@ -57,7 +65,27 @@ export default {
   name: "Repositories",
   data(){
     return{
-      repositories: repositoriesData
+      repositories: repositoriesData,
+      headers: [
+        { text: 'Location', value: 'location' },
+        { text: 'Name', value: 'name' },
+        { text: 'Languages', value: 'languages' },
+        { text: 'Tooling', value: 'tools' }
+      ]
+    }
+  },
+  computed: {
+    mainRepositories: function () {
+      return repositoriesData['main'].map(function(value,index){
+        console.log(value);
+        return {
+          url: value.url,
+          location: value.url.includes('gerrit.wikimedia.org') ? 'gerrit' : ( value.url.includes('github.com') ? 'github' : 'unknown' ),
+          name: value.url.replace('https://gerrit.wikimedia.org/r/admin/repos/','').replace('https://github.com/',''),
+          languages:"tba...",
+          tools:"tba...",
+          }
+      })
     }
   }
 };
