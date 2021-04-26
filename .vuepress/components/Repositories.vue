0d70e6ed5a31c966f2971f0f509eb30fe636e6c9
addshore <addshore@users.noreply.github.com>
2021-04-26 09:08:04 +0100

    More advanced repo list (#7)
    * Repo list -> Table with details
    
    * Use vuetify data table...
    
    * Entierly use vuetify data table...
    
    * Get data from github API for repos
    
    * Complete main layout from github api
    
    * Fresh npm i

--- .vuepress/components/Repositories.vue
+++ .vuepress/components/Repositories.vue
@@ -1,24 +1,64 @@
 <template>
   <div>
 
-    <h2>Ours</h2>
-    <table>
-      <tr>
-        <th>Location</th>
-        <th>Repository</th>
-      </tr>
-    <tr v-for="data in repositories['main']">
-      <td>
-        <a v-if="data.includes('gerrit.wikimedia.org')" v-bind:href="data">
+    <v-data-table
+      :headers="headers"
+      :dense=true
+      :disable-pagination=true
+      :items="mainRepositories"
+      class="elevation-1"
+    >
+      <template v-slot:item.location="{ item }">
+        <a v-if="item.location === 'gerrit'" v-bind:href="item.url">
           <img width="20" height="20" src="https://gerrit.wikimedia.org/favicon.ico"></img> Gerrit
           </a>
-        <a v-if="data.includes('github.com')" v-bind:href="data">
+        <a v-if="item.location === 'github'" v-bind:href="item.url">
           <img width="20" height="20" src="https://github.com/favicon.ico"></img> Github
-          </a>
-      </td>
-      <td><a v-bind:href="data">{{data.replace('https://gerrit.wikimedia.org/r/admin/repos/','').replace('https://github.com/','')}}</a></td>
-    </tr>
-    </table>
+        </a>
+      </template>
+      <template v-slot:item.name="{ item }">
+        <a v-bind:href="item.url" v-bind:title="item.description">
+          {{item.name}}
+        </a>
+      </template>
+
+      <template v-slot:item.primaryLanguage="{ item }">
+        <img v-if="item.primaryLanguage == 'PHP'" width="20" height="20" src="https://php.net/favicon.ico">
+        <img v-if="item.primaryLanguage == 'JavaScript'" width="20" height="20" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Javascript-736400_960_720.png/20px-Javascript-736400_960_720.png">
+        <img v-if="item.primaryLanguage == 'TypeScript'" width="20" height="20" src="https://www.typescriptlang.org/favicon.ico">
+        <img v-if="item.primaryLanguage == 'Python'" width="20" height="20" src="https://www.python.org/favicon.ico">
+        <img v-if="item.primaryLanguage == 'Vue'" width="20" height="20" src="https://vuejs.org/images/icons/favicon-32x32.png">
+        <img v-if="item.primaryLanguage == 'Ruby'" width="20" height="20" src="https://www.ruby-lang.org/favicon.ico">
+        <img v-if="item.primaryLanguage == 'Shell'" width="20" height="20" src="https://bashlogo.com/img/symbol/png/full_colored_dark.png">
+        {{item.primaryLanguage}}
+      </template>
+
+      <template v-slot:item.tools.npm="{ item }">
+        <img v-if="item.tools.npm" width="20" height="20" src="https://static.npmjs.com/b0f1a8318363185cc2ea6a40ac23eeb2.png">
+      </template>
+      <template v-slot:item.tools.composer="{ item }">
+        <img v-if="item.tools.composer" width="20" height="20" src="https://getcomposer.org/favicon.ico">
+      </template>
+      <template v-slot:item.tools.grunt="{ item }">
+        <img v-if="item.tools.grunt" width="20" height="20" src="https://gruntjs.com/img/favicons/favicon.ico">
+      </template>
+      <template v-slot:item.tools.phpunit="{ item }">
+        <img v-if="item.tools.phpunit" width="20" height="20" src="https://phpunit.de/favicon-32x32.png">
+      </template>
+      <template v-slot:item.tools.travis="{ item }">
+        <img v-if="item.tools.travis" width="20" height="20" src="https://travis-ci.com/images/logos/TravisCI-Mascot-3.svg">
+      </template>
+      <template v-slot:item.tools.github="{ item }">
+        <img v-if="item.tools.github" width="20" height="20" src="https://github.com/favicon.ico">
+      </template>
+      <template v-slot:item.tools.scrutinizer="{ item }">
+        <img v-if="item.tools.scrutinizer" width="20" height="20" src="https://scrutinizer-ci.com/favicon.ico">
+      </template>
+      <template v-slot:item.tools.phpcs="{ item }">
+        <span v-if="item.tools.phpcs">✔️</span>
+      </template>
+
+    </v-data-table>
 
     <h2>Others</h2>
     <ul>
@@ -30,15 +70,64 @@
 
 <script>
 import repositoriesData from './../../data/repositories.json'
+import githubRepositoryData from './../../data/github.json'
+
 export default {
   name: "Repositories",
   data(){
     return{
-      repositories: repositoriesData
+      repositories: repositoriesData,
+      headers: [
+        { text: 'Location', value: 'location'},
+        { text: 'Name', value: 'name' },
+        { text: 'Language', value: 'primaryLanguage' },
+        { text: 'Github', value: 'tools.github', class: 'vertical' },
+        { text: 'Travis', value: 'tools.travis', class: 'vertical' },
+        { text: 'Scrutinizer', value: 'tools.scrutinizer', class: 'vertical' },
+        { text: 'Composer', value: 'tools.composer', class: 'vertical' },
+        { text: 'PHPUnit', value: 'tools.phpunit', class: 'vertical' },
+        { text: 'PHPCS', value: 'tools.phpcs', class: 'vertical' },
+        { text: 'NPM', value: 'tools.npm', class: 'vertical' },
+        { text: 'Grunt', value: 'tools.grunt', class: 'vertical' },
+      ]
+    }
+  },
+  computed: {
+    mainRepositories: function () {
+      return repositoriesData['main'].map(function(value){
+        let githubData = githubRepositoryData[value.github].data.repository;
+        let mainUrl = value.url ? value.url : value.github;
+        let rootRepoFiles = githubData.defaultBranchRef.target.tree.entries.map(function(value) {
+          return value.name
+        });
+
+        return {
+          url: mainUrl,
+          location: mainUrl.includes('gerrit.wikimedia.org') ? 'gerrit' : ( mainUrl.includes('github.com') ? 'github' : 'unknown' ),
+          name: mainUrl.replace('https://gerrit.wikimedia.org/r/admin/repos/','').replace('https://github.com/',''),
+          primaryLanguage : githubData.primaryLanguage ? githubData.primaryLanguage.name : null,
+          description : githubData.description,
+          tools: {
+            npm: rootRepoFiles.indexOf('package.json') > -1,
+            composer: rootRepoFiles.indexOf('composer.json') > -1,
+            grunt: rootRepoFiles.indexOf('Gruntfile.js') > -1,
+            phpunit: rootRepoFiles.indexOf('phpunit.xml.dist') > -1,
+            travis: rootRepoFiles.indexOf('.travis.yml') > -1,
+            github: rootRepoFiles.indexOf('.github') > -1,
+            scrutinizer: rootRepoFiles.indexOf('.scrutinizer.yml') > -1,
+            phpcs: rootRepoFiles.indexOf('.phpcs.xml') > -1,
+          },
+        }
+      })
     }
   }
 };
 </script>
 
-<style scoped>
+<style>
+
+th.vertical > span {
+  writing-mode: vertical-rl;
+  text-orientation: sideways;
+}
 </style>
---
+++ bin/gen-github-repo-data.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# Fetches data that the team site needs for a single github repository
+# storing it in the data/github directory...
+
+IFS='/'
+read -a parts <<< "$1"
+IFS=' '
+
+SCRIPTDIR=$(dirname "$(readlink -f "$0")")
+OUTFILE=${SCRIPTDIR}/../data/tmp-github-${parts[0]}-${parts[1]}.json
+
+echo Fetching data for ${parts[0]} ${parts[1]}
+echo And outputing to ${OUTFILE}
+
+gh api graphql -f query="
+{
+  repository(owner: \"${parts[0]}\", name: \"${parts[1]}\") {
+    url
+    name
+    nameWithOwner
+    description
+    descriptionHTML
+    primaryLanguage {
+      name
+    }
+    languages(first: 5) {
+      nodes {
+        name
+      }
+    }
+    defaultBranchRef {
+      target {
+        ... on Commit {
+          tree {
+            entries {
+              name
+            }
+          }
+        }
+      }
+    }
+  }
+}
+" | jq '.' > $OUTFILE
