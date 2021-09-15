8ad641ed646e9be65bbe71380e2c5edcf58ace40
Tonina <tonina.zhelyazkova@wikimedia.de>
2021-09-15 16:58:17 +0200

    Add explanation of the tech prio sheet columns (#33)

---
+++ .vuepress/components/TechPrioSheetTable.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <table style="width: 100%">
+      <tr>
+        <th>Column</th>
+        <th>Explanation</th>
+      </tr>
+      <tr v-for="(column, index) in columns" v-bind:key="index">
+        <td>{{ column.name }}</td>
+        <td>{{ column.descr }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "TechPrioSheetTable",
+  props: [ "columns" ],
+};
+</script>
