c2e3589e8d4acb4e74dfce5016162cefd6a8ce96
Kara <71603938+quetzlthethird@users.noreply.github.com>
2021-11-08 16:22:35 +0100

    added route for projects/misc

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -52,6 +52,7 @@ module.exports = {
                 [ './', 'Projects Overview' ],
                 [ './wikibase', 'Wikibase' ],
                 [ './wikidata', 'Wikidata' ],
+                [ './misc', 'Miscellaneous' ],
             ],
             '/decisions/': [
                 [ './', 'Decisions' ],
